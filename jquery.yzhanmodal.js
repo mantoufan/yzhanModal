5d915823e358682b55ff45029d42e361516d5a34
Shon Ng <swu@junglescout.com>
2021-04-23 11:01:17 +0800

    :new: update the 0.0.1

--- jquery.yzhanmodal.js
+++ jquery.yzhanmodal.js
@@ -1,49 +1,54 @@
+/** jquery.yzhanmodal.js 0.0.1 author: Shon Ng Date: 2021-04-23 */
 (function ($) {
   var PREFIX = yzhanPrefix || 'yzhan'
   var $modal, $modalHeader, $modalHeaderCloseBtn, $modalBody, $modalIframe
 
   function initDom() {
-    $modal = $('<div>').addClass(PREFIX  + '-modal'),
-    $modalHeader = $('<div>').addClass(PREFIX  + '-modal-header'),
-    $modalHeaderTitle = $('<div>').addClass(PREFIX  + '-modal-header-title'),
-    $modalHeaderCloseBtn = $('<div>').addClass(PREFIX  + '-modal-header-close-btn'),
-    $modalBody = $('<div>').addClass(PREFIX  + '-modal-body'),
-    $modalIframe = $('<div>').addClass(PREFIX  + '-modal-iframe')
+    $modal = $('<div>').addClass(PREFIX  + '-modal yzhan-modal'),
+    $modalHeader = $('<div>').addClass(PREFIX  + '-modal-header yzhan-modal-header'),
+    $modalHeaderTitle = $('<div>').addClass(PREFIX  + '-modal-header-title yzhan-modal-header-title'),
+    $modalHeaderCloseBtn = $('<div>').addClass(PREFIX  + '-modal-header-close-btn yzhan-modal-header-close-btn').html('X'),
+    $modalBody = $('<div>').addClass(PREFIX  + '-modal-body yzhan-modal-body'),
+    $modalIframe = $('<iframe>').addClass(PREFIX  + '-modal-iframe yzhan-modal-iframe')
+
     // Form the DOM
-    $modalHeader.append($modalHeaderTitle).append($modalHeaderCloseBtn)
+    $modalHeader.append($modalHeaderCloseBtn).append($modalHeaderTitle)
     $modalBody.append($modalIframe)
     $modal.append($modalHeader).append($modalBody)
+    $('body').append($modal)
   }
 
   function initListener() {
     // show Modal
     $modal.show = function() {
-      $this.show()
+      $(this).show()
     }
 
     // hide Modal
     $modal.hide = function() {
-      $this.hide()
+      $(this).hide()
     }
 
     // close Modal
     $modal.close = function() {
-      $this.remove()
+      $(this).remove()
     }
 
     // close Modal with Btn
     $modalHeaderCloseBtn.on('click', function() {
-      $this.parent().parent().close()
+      $(this).parent().parent().remove()
     })
 
     // drag HeaderBar
     $modalHeader.on('mousedown', function(e) {
-      var offsetX = $modal.offset().left
-      var offsetY = $modal.offset().top
+      var marginLeft = parseInt($modal.css('margin-left')) | 0
+      var marginTop = parseInt($modal.css('margin-top')) | 0
+      var offsetX = $modal.offset().left - marginLeft
+      var offsetY = $modal.offset().top - marginTop
       var startX = e.pageX
       var startY = e.pageY
 
-      $modal.on('mousemove', function(e) {
+      $(document).on('mousemove', function(e) {
         var curX = e.pageX
         var curY = e.pageY
 
@@ -54,23 +59,30 @@
       })
 
       $(document).on('mouseup', function() {
-        $modal.off('mousemove')
+        $(document).off('mousemove')
       })
     })
 
   }
 
-  $.fn[PREFIX + 'modal'].open = function (opt) {
-    var title = opt.title
-    var url = opt.url
-    if (title && url) {
-      initDom()
-      initListener()
-
-      $modalHeaderTitle.html(title)
-      $modalIframe.attr('src', url)
+  $.extend({[PREFIX + 'Modal']: function (opt) {
+    function open (opt) {
+      var title = opt.title
+      var url = opt.url
+      if (title && url) {
+        initDom()
+        initListener()
+  
+        $modalHeaderTitle.html(title)
+        $modalIframe.attr('src', url)
+      }
+      return $modal
     }
-    return $modal
-  }
+
+    return {
+      open: open
+    }
+  }})
+  
 
 })(jQuery)
